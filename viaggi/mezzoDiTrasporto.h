c083aba3f937a2009084518b5fc0108a1f3d6cbb
DimoTheBest <64138187+DimoTheBest@users.noreply.github.com>
2020-07-28 18:05:49 +0200

    mezzoDiTrasporto

---
+++ viaggi/mezzoDiTrasporto.h
@@ -0,0 +1,23 @@
+#ifndef MEZZODITRASPORTO_H
+#define MEZZODITRASPoRTO_H
+
+#include <iostream>
+
+using namespace std;
+
+class mezzoDiTrasporto
+{
+public:
+	mezzoDiTrasporto();
+	virtual~mezzoDiTrasporto() {};
+
+private:
+
+};
+
+mezzoDiTrasporto::mezzoDiTrasporto()
+{
+}
+
+
+#endif // !MEZZODITRASPORTO_H
